553c65b3f7ffb8eb00396e20c7bc29078008d045
KayoSena <kayosenacoach@gmail.com>
2026-03-24 14:58:15 -0300

    atualizar

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -4,10 +4,6 @@ import type { Database } from './types';
 const SUPABASE_URL = "https://zbnkitesgcvkqbidwqmj.supabase.co"; 
 const SUPABASE_KEY = "sb_publishable_pgrZbNzKYPEWKRTEjFlHZQ_XGFb3s5Z";
 
-if (!SUPABASE_URL || SUPABASE_URL === "https://zbnkitesgcvkqbidwqmj.supabase.co") {
-  console.error("ERRO GRAVE: Você esqueceu de colar o URL no client.ts!");
-}
-
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_KEY, {
   auth: {
     storage: localStorage,
